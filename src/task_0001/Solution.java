f3ab489a695aff82719d623882e5b7e44db9ad80
Iurii Romanov <yromanov0@gmail.com>
2025-03-01 16:01:00 +0100

    task 0001: hash table solution

--- src/task_0001/Solution.java
+++ src/task_0001/Solution.java
@@ -1,12 +1,20 @@
 package task_0001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i<nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if ((nums[i] + nums[j])==target){
-                    return new int[] { i, j };
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            Integer complementId = map.get(complement);
+            if (complementId != null && complementId != i) {
+                return new int[] { i, complementId };
             }
         }
 
