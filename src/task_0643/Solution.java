3620c6ae0fbda10f7dd799f5f5db927e01d255c4
Iurii Romanov <yromanov0@gmail.com>
2025-03-06 18:01:20 +0100

    task 0643: reuse calculated sum

--- src/task_0643/Solution.java
+++ src/task_0643/Solution.java
@@ -2,19 +2,18 @@ package task_0643;
 
 public class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double maxAverage = -10000;
+        double currentSum = 0;
 
-        for (int i = 0; i < nums.length - k + 1; i++) {
-            double sum = 0;
-            for (int j = 0; j < k; j++) {
-                sum += nums[i + j];
-            }
-            double average = sum / k;
-            if (average > maxAverage) {
-                maxAverage = average;
-            }
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
+        double maxSum = currentSum;
+
+        for (int i = k; i < nums.length; i++) {
+            currentSum = currentSum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, currentSum);
         }
 
-        return maxAverage;
+        return maxSum / k;
     }
 }
