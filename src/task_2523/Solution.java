a1676d92799f5bc8388af1578e91c682e809b1a3
Iurii Romanov <yromanov0@gmail.com>
2025-03-07 10:29:56 +0100

    task 2523: optimised Eratosthenes

--- src/task_2523/Solution.java
+++ src/task_2523/Solution.java
@@ -4,14 +4,35 @@ import java.util.*;
 
 public class Solution {
     public int[] closestPrimes(int left, int right) {
+        ArrayList<Integer> primes = getPrimes(left, right);
+
+        int firstPrime = -1;
+        int secondPrime = -1;
+        int minimumSum = Integer.MAX_VALUE;
+
+        if (primes.size() > 1) {
+            for (int i = 0; i < primes.size() - 1; i++) {
+                int currentSum = primes.get(i + 1) - primes.get(i);
+                if (currentSum < minimumSum) {
+                    minimumSum = currentSum;
+                    firstPrime = primes.get(i);
+                    secondPrime = primes.get(i + 1);
+                }
+            }
+        }
+
+        return new int[]{firstPrime, secondPrime};
+    }
+
+    private static ArrayList<Integer> getPrimes(int left, int right) {
         boolean[] primeNumbers = new boolean[right + 1];
         for (int i = 2; i <= right; i++) {
             primeNumbers[i] = true;
         }
 
-        for (int i = 2; i <= right; i++) {
+        for (int i = 2; i * i <= right; i++) {
             if (primeNumbers[i]) {
-                for (int j = 2 * i; j <= right; j += i) {
+                for (int j = i * i; j <= right; j += i) {
                     primeNumbers[j] = false;
                 }
             }
@@ -24,24 +45,6 @@ public class Solution {
                 primes.add(i);
             }
         }
-
-        if (primes.size() < 2) {
-            return new int[]{-1, -1};
-        }
-
-        int firstPrime = -1;
-        int secondPrime = -1;
-        int minimumSum = Integer.MAX_VALUE;
-
-        for (int i = 0; i < primes.size() - 1; i++) {
-            int currentSum = primes.get(i + 1) - primes.get(i);
-            if (currentSum < minimumSum) {
-                minimumSum = currentSum;
-                firstPrime = primes.get(i);
-                secondPrime = primes.get(i + 1);
-            }
-        }
-
-        return new int[]{firstPrime, secondPrime};
+        return primes;
     }
 }
