0c4391afbc8efc1e0d7dc8d3978ccf971c75a9e0
Iurii Romanov <yromanov0@gmail.com>
2025-03-07 14:59:46 +0100

    task 0933: queue

---
+++ src/task_0933/Solution.java
@@ -0,0 +1,20 @@
+package task_0933;
+
+import java.util.*;
+
+class RecentCounter {
+
+    private final Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.add(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
