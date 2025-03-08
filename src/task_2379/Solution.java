5122990a4e2fb1351ed9e3f967839ff0286e3348
Iurii Romanov <yromanov0@gmail.com>
2025-03-08 13:15:47 +0100

    task 2379: reverted color

--- src/task_2379/Solution.java
+++ src/task_2379/Solution.java
@@ -1,40 +1,36 @@
 package task_2379;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 // Idea: find a k-sized window with most black blocks
 
 public class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int currentBlackBlocksCount = 0;
+        int minWhiteBlocks = 0;
 
         for (int i = 0; i < k; i++) {
-            if (blocks.charAt(i) == 'B') {
-                currentBlackBlocksCount++;
+            if (blocks.charAt(i) == 'W') {
+                minWhiteBlocks++;
             }
         }
 
-        int maxBlackBlocksCount = currentBlackBlocksCount;
+        int currentWhiteBlocks = minWhiteBlocks;
 
         for (int i = 1; i < blocks.length() - k + 1; i++) {
-            if (blocks.charAt(i - 1) == 'B') {
-                currentBlackBlocksCount--;
+            if (blocks.charAt(i - 1) == 'W') {
+                currentWhiteBlocks--;
             }
-            if (blocks.charAt(i + k - 1) == 'B') {
-                currentBlackBlocksCount++;
+            if (blocks.charAt(i + k - 1) == 'W') {
+                currentWhiteBlocks++;
             }
 
-            if (currentBlackBlocksCount > k) {
+            if (currentWhiteBlocks == 0) {
                 return 0;
             }
 
-            if (currentBlackBlocksCount > maxBlackBlocksCount) {
-                maxBlackBlocksCount = currentBlackBlocksCount;
+            if (currentWhiteBlocks < minWhiteBlocks) {
+                minWhiteBlocks = currentWhiteBlocks;
             }
         }
 
-        return k - maxBlackBlocksCount;
+        return currentWhiteBlocks;
     }
 }
