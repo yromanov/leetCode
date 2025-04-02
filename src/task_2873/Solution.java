4ec84d60e60a0cbdc52616e98f64c501d3096770
Iurii Romanov <yromanov0@gmail.com>
2025-04-02 15:26:58 +0200

    task 2873: greedy

---
+++ src/task_2873/Solution.java
@@ -0,0 +1,15 @@
+package task_2873;
+
+public class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long res = 0;
+        long imax = 0;
+        long dmax = 0;
+        for (int num : nums) {
+            res = Math.max(res, dmax * num);
+            dmax = Math.max(dmax, imax - num);
+            imax = Math.max(imax, num);
+        }
+        return res;
+    }
+}
