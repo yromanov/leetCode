e2486cdee73a39b895572556a30f75b5392903df
Iurii Romanov <yromanov0@gmail.com>
2025-05-06 10:13:51 +0200

    task 1128: optimisation

--- src/task_1128/Solution.java
+++ src/task_1128/Solution.java
@@ -1,22 +1,19 @@
 package task_1128;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
+        Map<Integer, Integer> seenDominos = new HashMap<>();
         int result = 0;
-        int length = dominoes.length;
-        for(int i = 0; i < length; i++) {
-            int[] domino = dominoes[i];
-
-            for(int j = i+1; j < length; j++) {
-                int[] domino2 = dominoes[j];
-
-                if((domino[0] == domino2[0] && domino[1] == domino2[1])
-                        || (domino[0] == domino2[1] && domino[1] == domino2[0])) {
-                    result++;
-                }
-            }
+        for (int[] domino : dominoes) {
+            int value = domino[0] > domino[1] ?
+                    domino[0] * 10 + domino[1] :
+                    domino[1] * 10 + domino[0];
+            int valueCount = seenDominos.getOrDefault(value, 0);
+            result += valueCount;
+            seenDominos.put(value, valueCount + 1);
         }
 
         return result;
